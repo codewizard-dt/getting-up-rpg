10005ebee1ded90e98605e076993586d62026fc7
David Taylor <94571813+codewizard-dt@users.noreply.github.com>
2022-08-08 12:51:26 -0500

    handleChoice and handleOutcome functions

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,18 @@
 async function handleChoice(ev) {
   const { target } = ev;
   const id = $(target).attr('id')
-  console.log(id)
+
   const result = await fetch(`/api/choices/${id}/random-outcome`)
   const outcome = await result.json()
-  console.log(outcome)
+
+  handleOutcome(outcome)
 }
 
+function handleOutcome(outcome) {
+  console.log(outcome)
+  $('#dialog-box').find('h2').html(outcome.randomOutcome.description)
+  $('#dialog-box').find('p').html(`Time change: ${outcome.randomOutcome.time_change}<br>Crisis change: ${outcome.randomOutcome.crisis_change}`)
+
+}
 $('.choice-btn').on('click', handleChoice)
 // $('#game-play').attr('src','/bathroom.html')
