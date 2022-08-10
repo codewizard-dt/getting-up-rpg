83142664b90d19eda57f5fbb6cd78c8c42cce6a8
Callie Williams <calliebwill@gmail.com>
2022-08-09 21:52:30 -0500

    should work??? (location triggers, dilemma ids updated)

--- seeds/choice-seeds.js
+++ seeds/choice-seeds.js
@@ -7,62 +7,62 @@ const choiceData = [
   },
   {
     description: `Get out of bed`,
-    dilemma_id: 1,
+    dilemma_id: 2,
   },
   {
     description: `Hit snooze`,
-    dilemma_id: 1,
+    dilemma_id: 3,
   },
   {
     description: `Ignore your phone`,
-    dilemma_id: 2,
+    dilemma_id: 4,
   },
   {
     description: `Chuckle and send the meme to your friend`,
-    dilemma_id: 2,
+    dilemma_id: 5,
   },
   {
     description: `Check out the comment section...`,
-    dilemma_id: 2,
+    dilemma_id: 6,
   },
   {
     description: 'Brush your teeth.',
-    dilemma_id: 3,
+    dilemma_id: 7,
   },
   {
     description: 'Take a shower.',
-    dilemma_id: 3,
+    dilemma_id: 8,
   },
   {
     description: 'Who are you performing for? (leave bathroom having done nothing)',
-    dilemma_id: 3,
+    dilemma_id: 9,
 
   },
 
   {
     description: 'Make yourself a coffee',
-    dilemma_id: 3,
+    dilemma_id: 10,
   },
 
   {
     description: 'Take your Prozac',
-    dilemma_id: 3,
+    dilemma_id: 11,
   },
   {
     description: 'Must you always rely on substances? (leave kitchen having done nothing)',
-    dilemma_id: 3,
+    dilemma_id: 12,
   },
   {
     description: 'Start Class',
-    dilemma_id: 4,
+    dilemma_id: 13,
   },
   {
     description: 'One final task before class',
-    dilemma_id: 4,
+    dilemma_id: 14,
   },
   {
     description: 'Skip class',
-    dilemma_id: 4,
+    dilemma_id: 15,
   }
 
 ]
--- seeds/dilemma-seeds.js
+++ seeds/dilemma-seeds.js
@@ -10,20 +10,24 @@ const dilemmaData = [
   },
   {
     title: 'Beep beep',
-    description: `You come across the funniest meme on Reddit.`
+    description: `You come across the funniest meme on Reddit.`,
+    location: 'bedroom'
   },
   {
     title: '7:00 AM',
-    description: 'You have 180 minutes before your class starts'
+    description: 'You have 180 minutes before your class starts',
+    location: 'bedroom'
   },
   {
     title: 'Time is fickle, tricky, mistress.',
-    description: 'Your life is like that tacky hour glass on your nightstand, except worse, the hour glass can reset, inconsequentially, with no evidence of its previous bearings.'
+    description: 'Your life is like that tacky hour glass on your nightstand, except worse, the hour glass can reset, inconsequentially, with no evidence of its previous bearings.',
+    location: 'bedroom'
 
   },
   {
     title: 'So arise to meet the day.',
-    description: 'Or go back to sleep. Does it really matter anymore?'
+    description: 'Or go back to sleep. Does it really matter anymore?',
+    location: 'bedroom'
     //  potential for automatic game over, if you choose to go back asleep
   },
   // Choices for dilemma id 1.
@@ -32,32 +36,38 @@ const dilemmaData = [
   // bathroom dialogue 
   {
     title: "The bathroom could use a good cleaning at some point.",
-    description: 'Is it this weekend that your Mom is visiting?'
+    description: 'Is it this weekend that your Mom is visiting?',
+    location: 'bathroom'
   },
 
   {
     title: 'You do might have time to take a shower, at the very least lets get those teeth brushed.',
-    description: "Like your father said: 'Routine is important, listlessness is for losers and dope fiends'"
+    description: "Like your father said: 'Routine is important, listlessness is for losers and dope fiends'",
+    location: 'bathroom'
   },
 
   // kitchen dialogue  
   {
     title: "I need coffee if I'm going to get through my class today.",
-    description: 'Or maybe you should just get back in bed and cry? How long has it been since you last got laid?'
+    description: 'Or maybe you should just get back in bed and cry? How long has it been since you last got laid?',
+    location: 'kitchen'
   },
   {
     title: "or maybe I should take my prozac. I certainly couldn't do both.",
-    description: 'Your driving yourself further from yourself, hiding behind a cloud of Serotonin.'
+    description: 'Your driving yourself further from yourself, hiding behind a cloud of Serotonin.',
+    location: 'kitchen'
   },
   // office dialogue 
   {
     title: 'Before starting class...',
     description: 'You have time to do one last thing.'
+    location: 'office'
 
   },
   {
     title: 'Choose carefully...',
     description: 'The implications could be life changing.'
+    location: 'office'
   }
   // Maybe a dilemma menu with two randomly generated ids along with a proceed with class option.  
 
